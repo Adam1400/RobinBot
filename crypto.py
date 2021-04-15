a723c4f72e2046e318f364a2ba178ff4ecca29a8
Adam1400 <42878557+Adam1400@users.noreply.github.com>
2021-04-15 09:43:56 -0600

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -475,6 +475,7 @@ def get_gains():
         if(starting_amount < 6):
             starting_amount = get_crypto_entery(ticker) 
         
+        #returns daily gains (not total gains)
         gains = round((crypto_position(ticker) - starting_amount), 2)
         
     else:
